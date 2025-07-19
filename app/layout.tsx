123e3052fbe4d3654e6b419a96cc23807ac28e71
Dewmin Deniyegedara <dewminkasmitha30@gmail.com>
2025-07-20 01:28:51 +0530

    feat:add vantaJs

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          suppressHydrationWarning
+          src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+        ></script>
+        <script
+          suppressHydrationWarning
+          src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,49 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
 export default function Home() {
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    let vantaEffect: any;
+
+    if (
+      typeof window !== 'undefined' &&
+      (window as any).VANTA &&
+      (window as any).THREE
+    ) {
+      vantaEffect = (window as any).VANTA.NET({
+        el: "#vanta-bg", // Your selector
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        scale: 1.0,
+        scaleMobile: 1.0,
+      });
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, []);
+
   return (
-    <>
-      <h1> I am tryign to host a Next.js app on Vercel</h1>
-    </>
+    <div
+      id="vanta-bg"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '2rem',
+      }}
+    >
+      <h1>Tried VantaJs </h1>
+    </div>
   );
 }
